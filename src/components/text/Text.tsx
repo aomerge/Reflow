e9ef0e99267eaa5a83a3096068de7778d70256c8
aomerge <codenode.ortega@gmail.com>
2024-09-13 01:41:53 -0600

    Refactor Text component to use TextProps interface

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -2,12 +2,12 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import '../../styles/tailwind.css';
 
-interface ButtonProps {
+interface TextProps {
   label: string;
   type: String;
 }
 
-const Text: React.FC<ButtonProps> = ({ label, type }) => {
+const Text: React.FC<TextProps> = ({ label, type }) => {
     return (
         <text className="bg-slate-500 text-white py-2 px-4 m-2  rounded-sm hover:bg-blue-700">
             {label}
