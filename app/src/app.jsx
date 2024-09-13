c1819c821364146ca74af1114cc132288e0f139b
aomerge <codenode.ortega@gmail.com>
2024-09-12 23:02:41 -0600

    Refactor button component to use Tailwind CSS classes

--- app/src/app.jsx
+++ app/src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './button.v1.tsx';  // Ruta relativa al archivo compilado de la librería
+import Button from '../../src/components/button/button.v1';  // Importa el componente Button desde la librería
 
 
 
