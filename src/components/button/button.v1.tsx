cf36f53dad903d5835bc4963e14eac0813ffdf24
aomerge <codenode.ortega@gmail.com>
2024-09-13 04:19:02 -0600

    Refactor Button component to accept style prop

--- src/components/button/button.v1.tsx
+++ src/components/button/button.v1.tsx
@@ -4,12 +4,13 @@ import '../../styles/tailwind.css';
 
 interface ButtonProps {
   label: string;
+  style?: React.CSSProperties;
 }
 
-const Button: React.FC<ButtonProps> = ({ label }) => {
+const Button: React.FC<ButtonProps> = ({ label, style }) => {
     
     return (
-        <button className="bg-slate-500 text-white py-2 px-4 m-2  rounded-sm hover:bg-blue-700">
+        <button style={style} className="bg-slate-500 text-white py-2 px-4 m-2  rounded-sm hover:bg-blue-700">
             {label}
         </button>
         );
