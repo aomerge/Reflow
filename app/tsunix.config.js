82d5d51f77f4c8ea0df4d528002fe2f96ad556b0
aomerge <codenode.ortega@gmail.com>
2024-09-13 04:16:23 -0600

    Refactor HTML files to improve color palette generation and background color prediction

---
+++ app/tsunix.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    svg:{
+        input: path.resolve(__dirname, 'src/svg'),        
+    },
+    color : {
+        primary: '#ff0000',
+        secondary: '#00ff00',
+        tertiary: '#0000ff'        
+    }
+    
+};
