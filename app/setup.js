9a835dfabcb1e18201c86e7174c549df4b407e26
aomerge <codenode.ortega@gmail.com>
2024-09-14 23:27:22 -0600

    Remove setup.js, app.jsx, and button.v1.tsx files to clean up unused components

--- app/setup.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; 
-
-alert('Hola desde setup.js');
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>mama</h1>);
-
