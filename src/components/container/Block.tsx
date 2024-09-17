8ee1b4133022c083436d966b39617a3d0df7165f
aomerge <codenode.ortega@gmail.com>
2024-09-17 12:25:27 -0600

    Refactor Block component to implement context for managing outlet state; enhance type handling and element rendering; integrate CSSTransition for animations

--- src/components/container/Block.tsx
+++ src/components/container/Block.tsx
@@ -1,10 +1,23 @@
-import React, { Children, ReactNode } from 'react';
+import React, { createContext, useContext, HTMLAttributes, useState, Children, ReactElement, ReactNode, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import '../../styles/tailwind.css';
+import CSSTransition from '../trancition/CssTrancition';
+
+type ElementType = keyof JSX.IntrinsicElements;
 
 // Interface
+interface ContainerProps<T extends ElementType> extends HTMLAttributes<void>{
+  col_end?: number,
+  col_start?: number,
+  row_start?: number, 
+  row_end?: number, 
+  children: React.ReactNode;
+  type?: T;
+  style?: React.CSSProperties;
+  newElement?: React.ReactElement
+}
 
-interface BlockProps {
+interface BlockProps extends React.HTMLAttributes<HTMLElement> {   
   children: ReactNode;
   type?: string;
   col_start?: number;
@@ -12,116 +25,97 @@ interface BlockProps {
   row_start?: number;
   row_end?: number;
   style?: React.CSSProperties;
-  template?: string;
+  template?: string;     
+
 }
 
-
-
+interface BlockContextType {
+  outlet:  ReactNode;
+  setOutlet: any;
+}
 
 // Component
+export const BlockContext = createContext<BlockContextType | undefined>({ outlet: null, setOutlet: () => {} });
 
-const Container: React.FC<BlockProps> = (
-  { 
-    children, 
-    type,
-    col_start,
-    col_end,
-    row_start,
-    row_end,
-    style
-  }: BlockProps) => {
+export const useBlockContext = () => {
+  const context = useContext(BlockContext);
+  if (!context) {
+    throw new Error('useBlockContext must be used within a Block component');
+  }
+  return context;
+};
+
+const Block =<T extends ElementType = 'div'> ({   
+  newElement,
+  col_end,
+  col_start,
+  row_start, 
+  row_end, 
+  children, 
+  type, 
+  style, 
+  ...props }: ContainerProps<T>) => {
+
+    const [outlet, setOutlet] = useState<any>(children);       
+  
+    const handleClick = () => {
+      newElement && setOutlet?
+        setOutlet(React.createElement(newElement.type, { id: 1, rollBack }))        
+      : (console.error('newElement or setOutlet is not available'));
+    };
+
+    const rollBack = () => {
+      setOutlet(children);
+    };      
+
     const ClassContainer = `  
           ${col_start && 'col-start-'+col_start}
           ${col_end && 'col-end-'+col_end}
           ${row_start && 'row-start-'+row_start}
           ${row_end && 'row-end-'+row_end}
+
           `;
-  return (    
-    <>
-      {
-        type === undefined &&
-        <div style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </div>
-      }
-      {
-        type === 'Block' &&
-        <div style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </div>
-      }
-      {
-        type === 'Section' &&
-        <section style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </section>
-      }
-      {
-        type === 'Article' &&
-        <article style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </article>
-      }            
-      {
-        type === 'Aside' &&
-        <aside style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </aside>
-      }
-      {
-        type === 'Footer' &&
-        <footer style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </footer>
-      }      
-      {
-        type === 'Main' &&
-        <main style={style} className={ClassContainer}>
-          {
-            children
-          }
-        </main>
-      }            
-    </>    
-  )
+  
+    const Element = getElementByType(type) as React.ElementType;    
+
+  return (
+    <BlockContext.Provider value={{ outlet, setOutlet }}>   
+      <CSSTransition
+        in={!!outlet}
+        timeout={500}
+        classNames="fade"
+        unmountOnExit
+      >
+
+        <Element style={style} className={ClassContainer} {...props}>
+          {outlet ? (
+            <>
+              {Children.map(outlet, (child) => {
+                if (React.isValidElement(child)) {
+                  return React.cloneElement(child as ReactElement<any>, { handleClick, rollBack });
+                }
+                return outlet;
+              })}
+            </>
+          ) : (        
+            { outlet }
+          )}
+        </Element>
+        </CSSTransition>          
+    </BlockContext.Provider>
+  );
 }
 
-const Block: React.FC<BlockProps> = ( 
-  { 
-  children, 
-  type,
-  col_start,
-  col_end,
-  row_start,
-  row_end,
-  style
-}: BlockProps) => {
-      
-      return (
-        <>
-          <Container 
-            style={style} 
-            col_end={col_end} 
-            col_start={col_start} 
-            row_start={row_start}
-            row_end={row_end}  
-            type={type}
-            >
-            {children}
-          </Container>
-        </> );
+function getElementByType<T extends ElementType>(type?: T): T {
+  const elements: Record<string, ElementType> = {
+    block: 'div',
+    section: 'section',
+    article: 'article',
+    aside: 'aside',
+    footer: 'footer',
+    main: 'main',
+  };
+  return (elements[type as string] || 'div') as T;
 }
   
 export default Block;
