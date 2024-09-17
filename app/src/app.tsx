8266a8455caf208ef70267b3e6bd534a55a05c46
aomerge <codenode.ortega@gmail.com>
2024-09-16 22:21:35 -0600

    Refactor App component to integrate useFetchData for data fetching; update ChildComponent to display results and manage text state; add Submit component for form submission

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -1,48 +1,67 @@
 import React, { useState, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom';
-import Fetch from '../../src/components/inputs/Fetch';
+import Fetch, {useFetchData} from '../../src/components/inputs/Fetch';
 import Input from '../../src/components/inputs/Inputs';
+import Submit from '../../src/components/inputs/Submit';
+import Block from '../../src/components/container/Block';
 
 
-interface ChildProps {
-  data?: Post | null;
-  loading?: boolean;
-  error?: string | null;
+interface ChildProps {  
+  text: string | number;
+  setText: React.Dispatch<React.SetStateAction<any>>;
 }
 
-interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+interface Post{
+  results: Array<IPost>;
+}
+
+interface IPost{
+  name: string;
+  image: string;
 }
 
 
-const ChildComponent: React.FC<ChildProps> = ({ data, loading, error }) => {
-  const [ text, setText ] = useState<string>("");
-
+const ChildComponent: React.FC<ChildProps> = ({ text, setText }) => {
+  const { data, loading, error } = useFetchData<Post>();
+  
   if (loading) return <p>Cargando...</p>;
   if (error) return <p>Error: {error}</p>;
   if (!data) return <p>No hay datos disponibles</p>
-  
+
   return (
-    <div>
-      <h2>
-        {text}
-      </h2>
-      <h1>{data.title}</h1>
-      <span>{data.userId}</span>
-      <p>{data.body}</p>
-      <Input value={text} onValueChange={setText} type="Text" />
+    <div>    
+     {
+      data.results.map((item: any, index: number) => (
+        <div key={index}>
+          <h1>{item.name}</h1>
+          <img src={item.image} alt={item.name} />
+        </div>
+      ))
+     }
     </div>
   );
 };
 
-const App = () => {
+const App = () => {  
   const [data, setData] = useState<Post | null>(null);
+  const [ text, setText ] = useState<number>(1);
+
   return (
-    <Fetch<Post>  url="https://jsonplaceholder.typicode.com/posts/1" option={{ method: 'GET' }} >
-      <ChildComponent />
+    <Fetch<Post>  url={`https://rickandmortyapi.com/api/character`} option={
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'GET',
+        body: null
+      }
+    }  >
+      <div>
+        <h1>abs {text}</h1>
+        <ChildComponent text={text} setText={setText} />
+        <Input  value={text} onValueChange={setText} type="number"  />   
+        <Submit Text="Send" style={{backgroundColor: 'blue', color: 'white'}} />
+      </div>
     </Fetch>
   );
 };
