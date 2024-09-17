9aad47ae1c14f2e943bc2cce835ac3e009dc6bcc
aomerge <codenode.ortega@gmail.com>
2024-09-16 22:21:20 -0600

    Enhance Input component to support numeric values and custom key events; integrate useFetchData for refresh functionality

--- src/components/inputs/Inputs.tsx
+++ src/components/inputs/Inputs.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useFetchData } from './Fetch'
+import '../../styles/tailwind.css';
 
 interface FetchProps {  
     template?: string;  
-    value: string; 
+    value: string | number; 
+    eventKey?: string;
     onValueChange: React.Dispatch<React.SetStateAction<any>>;
     type?: string; 
+    style?: React.CSSProperties;
   }
 
 const Input : React.FC<FetchProps>  = (
@@ -13,13 +17,22 @@ const Input : React.FC<FetchProps>  = (
         template,
         value,
         onValueChange,
-        type
+        eventKey,
+        type,
+        style
     }
 ) => {
+    const { reFresh } = useFetchData<any>();  
+
+    const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        eventKey && event.key === eventKey && reFresh();        
+    }
 
     return (
-        <div>
-            <input 
+        <div style={style}  className='overflow-hidden border m-2 rounded-md font-bold shadow'>                    
+            <input
+                onKeyDown={onKeyDown}
+                className=' w-full h-full px-2'                
                 value={value}
                 type={type} 
                 onChange={(event) => onValueChange(event.target.value)} 
