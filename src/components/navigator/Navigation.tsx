2127ab25455e0d9ad9c826c95339f3f732b820fa
aomerge <codenode.ortega@gmail.com>
2024-09-24 21:12:57 -0600

    Refactor Navigation component to use INavigatorProps interface and update theme rendering

--- src/components/navigator/Navigation.tsx
+++ src/components/navigator/Navigation.tsx
@@ -1,36 +1,44 @@
 import React, { Children, ReactNode } from 'react';
 import ReactDOM from 'react-dom';
 import '../../styles/tailwind.css';
+import Button from '../button/button.v1';
+import themesConfig from '../templates/themesConfig';
+import { INavigatorProps } from './INavigation';
 
-interface NavigatorProps {
-  children: ReactNode;
-  type?: string;
-  col_start?: number;
-  col_end?: number;
-  row_start?: number;
-  row_end?: number;
-  style?: React.CSSProperties;
-  template?: string;
-}
-
-const Navigation: React.FC<NavigatorProps> = ( 
+const Navigation: React.FC<INavigatorProps> = ( 
     { 
-    children, 
-    type,
+    Url,
+    template,
+    children,     
     col_start,
     col_end,
     row_start,
     row_end,
-    style
-  }: NavigatorProps) => {
+    style,
+    className,
+    ...props
+  }: INavigatorProps) => {
+      const [theme, setTheme] = React.useState<keyof typeof themesConfig>(template || 'basic');
+      const themeConfig = themesConfig[theme];
+
+      const renderThemeComponent = () => {
+        if (React.isValidElement(themeConfig.component)) {
+          // Clonamos el componente del tema y le pasamos las props y children
+          return React.cloneElement(themeConfig.component as React.ReactElement, { className, col_start, col_end, row_start, row_end, style, ...props, children });
+        } else if (typeof themeConfig.component === 'function') {
+          // Si es una función, ejecutamos la función pasando las props y children
+          return themeConfig.component({ className, col_start, col_end, row_start, row_end, style, ...props, children });
+        }
+        return themeConfig.component;
+      };
         
         return (
           <>
-            <nav>
+            <header className={`flex justify-between ${className}`} {...props}>              
                 {
-                    children
+                  renderThemeComponent()
                 }
-            </nav>            
+            </header>            
           </> );
   }
     
