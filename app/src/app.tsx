726f5ef66164199029e8f70adbe57449191e8c96
aomerge <codenode.ortega@gmail.com>
2024-09-24 21:13:17 -0600

    Refactor components and update styles and theme rendering

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -5,7 +5,9 @@ import Fetch, {useFetchData} from '../../src/components/inputs/Fetch';
 import Input from '../../src/components/inputs/Inputs';
 import Submit from '../../src/components/inputs/Submit';
 import Grid from '../../src/components/container/Grid';
+import Text from '../../src/components/text/Text';
 import Block, {BlockContext, useBlockContext} from '../../src/components/container/Block';
+import Navigation from '../../src/components/navigator/Navigation';
 
 
 interface ChildProps {  
@@ -22,87 +24,21 @@ interface IPost{
   image: string;
 }
 
-
-const ElementTestT = ({ setId, data }: any)=>{   
-  return (
-    <Grid col={5} row={0} >
-    {
-      data.results.map((item: any, index: number) => {
-      
-        return (
-        <Block onClick={()=>setId(index+1)} style={{borderRadius:"26px"}}  key={index}>
-          <h1>{item.name}</h1>
-          <img src={item.image} alt={item.name} />
-        </Block>    
-        ) 
-      })
-    }
-    </Grid>
-  )
-}
-
-const NewElement = () => {
-  const { data, loading, error } = useFetchData<any>();  
-  return (
-    <div>
-      <h1>{data?.name}</h1>
-      <img src={data?.image} alt={data?.name} />
-    </div>
-  );
-}
-
-const NewElementT = ({Id ,rollBack}: any) => {    
-  const { data, loading, error } = useFetchData<any>();    
-  return (
-    <div>
-        <Block>
-          <Fetch url={`https://rickandmortyapi.com/api/character/${Id}`}>
-            <NewElement  />
-          </Fetch>
-          <Block >
-            <Button className='mt-2' onClick={rollBack} label='Volver' />
-          </Block>
-        </Block>
-    </div>    
-  );
-};
-
-const ChildComponent: React.FC<ChildProps> = ({ text, setText }) => {
-  const [Id, setId] = useState<number | null>(null);
-  const { data, loading, error } = useFetchData<Post>();     
-
-  if (loading) return <p>Cargando...</p>;
-  if (error) return <p>Error: {error}</p>;
-  if (!data) return <p>No hay datos disponibles</p>  
-  
-  return (
-    <Block type='main'>  
-      <Block
-        type='section'
-        newElement={
-          Id !== null ? (
-            <NewElementT Id={Id} setId={setId} rollBack={() => setId(null)} />
-          ) : undefined
-        }
-      >
-        <ElementTestT setId={setId} data={data} />
-      </Block>           
-    </Block>
-  );
-};
-
 const App = () => {  
   const [data, setData] = useState<Post | null>(null);
   const [ text, setText ] = useState<number>(0);
 
   return (
-    <Fetch<Post>  url={`${
-      text === 0 ? 'https://rickandmortyapi.com/api/character' : `https://rickandmortyapi.com/api/character/${text}`
-    }`} >
-      <div style={{padding:"10px 20px"}}>        
-        <ChildComponent text={text} setText={setText} />        
-      </div>
-    </Fetch>
+    <Block type='section' style={{padding:"10px 20px"}}>        
+        <Navigation template='basic' >
+          <Button  type="button" label="Click Me" />
+          <Button  type="button" label="Click Me" />
+          <Button  type="button" label="Click Me" />
+        </Navigation>
+        <div>          
+          <Text label="Fetch Data" type="h1" />
+        </div>
+    </Block>
   );
 };
 
--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,18 +1,40 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 import '../../styles/tailwind.css';
+
+type ElementType = keyof JSX.IntrinsicElements;
 
 interface TextProps {
   label: string;
-  type: String;
+  type: ElementType;
 }
 
 const Text: React.FC<TextProps> = ({ label, type }) => {
-    return (
-        <text className="bg-slate-500 text-white py-2 px-4 m-2  rounded-sm hover:bg-blue-700">
-            {label}
-        </text>
-        );
-    }
+
+  const getElementClassName = (type: ElementType): string => {
+    const styles: Partial<Record<ElementType, string>> = {
+      h1: 'text-4xl font-bold ',
+      h2: 'text-3xl font-semibold ',
+      h3: 'text-2xl font-medium ',
+      span: 'text-base ',
+      p: 'text-lg ', 
+    };
+    return styles[type] || styles.p!;
+  };
+
+  const Element = getElementByType(type) as React.ElementType;
+  const className = getElementClassName(type); // Obtener las clases de Tailwind CSS
+
+  return <Element className={className}>{label}</Element>;
+};
 
 export default Text;
+
+function getElementByType<T extends ElementType>(type?: T): T {
+  const elements: Record<string, ElementType> = {
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    span: 'span',
+  };
+  return (elements[type as string] || 'p') as T;
+}
