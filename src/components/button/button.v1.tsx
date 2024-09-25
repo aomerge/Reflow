602025bcd9cceada62961ceca1d4003c281cda05
aomerge <codenode.ortega@gmail.com>
2024-09-24 21:12:42 -0600

    Refactor Button component to update color variables and remove console.log statement

--- src/components/button/button.v1.tsx
+++ src/components/button/button.v1.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../../styles/tailwind.css';
-import { getConfig } from '../../utils/config';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
 }
 
 const Button = ({ label, className, ...props }: ButtonProps) => {
-    console.log(getConfig().color.primary.toString());
-    const Primary = getConfig().color.primary.toString();
+    
+    
   return (
-    <button className={`px-4 py-1 bg-[${Primary}] text-white rounded-sm ${className}`} {...props}>
+    <button className={`px-4 py-1 bg-primary text-secondary rounded-sm ${className}`} {...props}>
       {label}
     </button>
   );
