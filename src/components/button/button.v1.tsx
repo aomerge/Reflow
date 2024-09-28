5885ee20634ea11815ded4a5a755683589adcc36
aomerge <codenode.ortega@gmail.com>
2024-09-27 22:20:33 -0600

    Refactor button component to wrap button element in a fragment

--- src/components/button/button.v1.tsx
+++ src/components/button/button.v1.tsx
@@ -9,9 +9,11 @@ const Button = ({ label, className, ...props }: ButtonProps) => {
     
     
   return (
-    <button className={`px-4 py-1 bg-primary text-secondary rounded-sm ${className}`} {...props}>
-      {label}
-    </button>
+    <>
+      <button className={`px-4 py-1 bg-primary text-secondary rounded-sm ${className}`} {...props}>
+        {label}
+      </button>
+    </>
   );
 };
 
