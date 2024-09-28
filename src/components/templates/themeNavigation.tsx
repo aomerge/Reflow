3341a7f6f6e6c0af7a05b7c2603bd9986266b2ed
aomerge <codenode.ortega@gmail.com>
2024-09-27 22:22:05 -0600

    Refactor themesConfig.tsx and add SubNavigator and ThemeNavigation components

---
+++ src/components/templates/themeNavigation.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+class ThemeNavigation extends React.Component {
+    static defaultProps = {};  
+}
--- src/components/templates/themesConfig.tsx
+++ src/components/templates/themesConfig.tsx
@@ -1,16 +1,32 @@
 // themesConfig.js
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../button/button.v1';
 import {INavigatorProps} from '../navigator/INavigation';
 
 const themesConfig = {
-  basic: {
+  basic:{
+    component: <nav>
+      <Button label="Home" />
+      <Button label="Projects" />
+      <Button label="About" />
+    </nav>
+  },
+  h: {
     styles: 'bg-white text-black',
     component: ({Url, children}: INavigatorProps)=>{
       return(
-        <div id='basic' className='flex w-full flex-col'>
-          <section>
-              <p>asamsa</p>
+        <div id='basic' className='flex w-full flex-col  py-4 p-6 bg-secondary mb-5  text-black'>
+          <section className='flex justify-between mb-10'>
+              <ul className='flex gap-5'>
+                <li><p>number: 477555555</p></li>
+                <li>site web</li>
+                <li></li>
+              </ul>
+              <ul className='flex gap-5'>
+                <li>facebook</li>
+                <li>twitter</li>
+                <li>instagram</li>
+              </ul>
           </section>
           <section className='flex w-full justify-between'>
               <div>
@@ -20,10 +36,20 @@ const themesConfig = {
               <nav className='gap-5 flex'>                
                   {
                     React.Children.map(children, (child, index) => {
-                      return React.cloneElement(child as React.ReactElement, { key: index, className: 'bg-tertiary text-white' });
+                      return React.cloneElement(child as React.ReactElement, { key: index, className: 'bg-black text-white roudend-md', onFocus:()=>{alert("hello")} });
                     })
                   }                                                    
               </nav>
+              <div>
+                <ul className='flex gap-5'>
+                  <li>
+                    hola
+                  </li>
+                  <li>
+                    min
+                  </li>
+                </ul>
+              </div>
           </section>
         </div>
       );
