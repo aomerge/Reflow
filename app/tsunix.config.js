89198e38b4fb1cb5e31813992aecbf30019f72ce
aomerge <codenode.ortega@gmail.com>
2024-09-27 22:20:22 -0600

    Refactor color variables in tsunix.config.js

--- app/tsunix.config.js
+++ app/tsunix.config.js
@@ -5,9 +5,13 @@ module.exports = {
         input: path.resolve(__dirname, 'src/svg'),        
     },
     color : {
-        primary: '#F2FEE2',
-        secondary: '#000F0F',
-        tertiary: '#0000ff'        
+        primary: '#B0FF40',
+        secondary: '#9AD948',
+        tertiary: '#0e5351',
+        quaternary: '#3D3D3D',   
+        quinary: '#B8B84D',
+        black: '#121212',
+        white: '#DEDEDE',     
     },
     template: {
         input: path.resolve(__dirname, 'src/template'),    
