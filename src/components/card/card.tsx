c5cc5c19ffe75b4a978254c0991f2468376d007e
aomerge <codenode.ortega@gmail.com>
2024-11-17 01:54:18 -0600

    Add grain SVG filter and enhance Card component with optional props and improved rendering logic

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -2,33 +2,38 @@ import React from 'react';
 import './card.css';
 
 interface CardProps {
-    title: string;
-    content: string;
+    title?: string;
+    content?: string;
     img?: string;
     template?: string | null | undefined;
+    className?: string;
+    Style?: React.CSSProperties;
+    children?: React.ReactNode;
 }
 
 const Card: React.FC<CardProps> = ({ title, content, template, ...props}) => {    
     const [action, direction , color ] = template ? template.split('-') : [null, null, null];
     console.log(action, direction, color);
     return (        
-        <div className={`${template? `card${"-"+template}` : "card"}`}>            
+        <div style={props.Style} className={`${template ? `card${"-" + template}` : "card"} ${props.className}`}>            
+            {action === 'custom' && props.children}                   
             {
-                action === 'img' && 
-                [
-                <img src={props.img} alt={title} /> ,
-                <section>
-                    <h2 className="card-title">{title}</h2>
-                    <p className="card-content">{content}</p>
+            action === 'img' && 
+            <>
+                <img key="img" src={props.img} alt={title} />
+                <section key="section">
+                <h2 className="card-title">{title}</h2>
+                <p className="card-content">{content}</p>
                 </section>
-                ]
+            </>
             }
             {
-                action === null && [
-                    <h2 className="card-title">{title}</h2>,
-                    <p className="card-content">{content}</p>
-                ]
-            }            
+            (action === null || action !== 'custom' && action !== 'img'  )   && 
+            <>
+                <h2 className="card-title">{title}</h2>
+                <p className="card-content">{content}</p>
+            </>
+            }
         </div>
     );
 };
