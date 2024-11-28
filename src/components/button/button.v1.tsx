3adf97a8d6bfd2b5afa236f19f5a9d0dd68a2ad1
aomerge <codenode.ortega@gmail.com>
2024-11-27 18:01:41 -0600

    Add Button component with customizable styles and animations

--- src/components/button/button.v1.tsx
+++ src/components/button/button.v1.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import '../../styles/tailwind.css';
+import './button.css';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
   element?: React.ReactElement;
+  template?: string;
 }
 
-const Button = ({ label, className, ...props }: ButtonProps) => {
-    
-    
+const Button: React.FC<ButtonProps> = ({ label, className, template, ...props }) => {
+  const [action, direction, color] = template ? template.split('-') : [null, null, null];  
+
   return (
     <>
-      <button className={`px-4 py-1 bg-primary text-black rounded-sm ${className}`} {...props}>
-        {label}
+      <button
+        id='button'
+        className={`${template ? `button-${template}` : 'button'} ${(className !== null || className !== undefined) ? className: " "}`}
+        {...props}
+      >
+        {action === 'icon' && props.children}
+        {action !== 'icon' && label}
       </button>
     </>
   );
