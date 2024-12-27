c571c7cc2a87de60ee385ff6d43a30008a59e1b1
aomerge <codenode.ortega@gmail.com>
2024-12-26 20:38:24 -0600

    Add Card component with customizable templates and styles, including button action

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
 import './card.css';
+import { CardProps } from './interface/Icard';
 
-interface CardProps {
-    title?: string;
-    content?: string;
-    img?: string;
-    template?: string | null | undefined;
-    className?: string;
-    Style?: React.CSSProperties;
-    children?: React.ReactNode;
-}
-
+/**
+ * Card component that displays content based on the provided template.
+ *
+ * @param {CardProps} props - The properties for the Card component.
+ * @param {string} props.title - The title of the card.
+ * @param {string} props.content - The content of the card.
+ * @param {string} [props.template] - The template string to determine the card's layout and style.
+ * @param {React.CSSProperties} [props.Style] - The inline styles for the card.
+ * @param {string} [props.className] - Additional class names for the card.
+ * @param {React.ReactNode} [props.children] - The custom content to be rendered if the template action is 'custom'.
+ * @param {string} [props.img] - The image source URL to be rendered if the template action is 'img'.
+ *
+ * @returns {JSX.Element} The rendered Card component.
+ */
 const Card: React.FC<CardProps> = ({ title, content, template, ...props}) => {    
     const [action, direction , color ] = template ? template.split('-') : [null, null, null];    
     return (        
-        <div style={props.Style} className={`${template ? `card${"-" + template}` : "card"} ${props.className}`}>            
+        <div id="card" style={props.Style} className={`${template ? `card${"-" + template}` : "card"} ${props.className}`}>            
             {action === 'custom' && props.children}                   
             {
             action === 'img' && 
             <>
                 <img key="img" src={props.img} alt={title} />
                 <section key="section">
-                <h2 className="card-title">{title}</h2>
-                <p className="card-content">{content}</p>
+                    <h2 className="card-title">{title}</h2>
+                    <p className="card-content">{content}</p>
                 </section>
             </>
             }
             {
-            (action === null || action !== 'custom' && action !== 'img'  )   && 
+            (action === null || action !== 'custom' && action !== 'img' && action !== 'button'  )   && 
             <>
                 <h2 className="card-title">{title}</h2>
                 <p className="card-content">{content}</p>
             </>
+            }
+            {
+                action === 'button' && 
+                <>
+                    <h2 className="card-title">{title}</h2>
+                    <p className="card-content">{content}</p>
+                    <button className={`button ${template ? `button-${template}` : ''}`} {...props}>Ver más</button>
+                </>
             }
         </div>
     );
---
+++ src/components/card/interface/Icard.ts
@@ -0,0 +1,9 @@
+export interface CardProps {
+    title?: string;
+    content?: string;
+    img?: string;
+    template?: string | null | undefined;
+    className?: string;
+    Style?: React.CSSProperties;
+    children?: React.ReactNode;
+}
